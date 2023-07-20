afec31cf439ae68c775c8cf09c893a4d2186119d
MisterCat100 <savingleb2007@mail.ru>
2023-07-20 17:56:44 +0300

    L1-T2 training naming + hi staff

---
+++ src/main/kotlin/Lesson1/task3.kt
@@ -0,0 +1,18 @@
+package Lesson1
+
+fun main() {
+    val flightYear = 1961
+    var hour = 9
+    var minute = 7
+    println(flightYear)
+    println(hour)
+    println(minute)
+    hour = 10
+    minute = 55
+    print(hour)
+    print(":")
+    println(minute)
+
+
+
+}
