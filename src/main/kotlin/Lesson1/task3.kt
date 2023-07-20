ad7e19c185d54f2698b9497c60f73e556aed3c67
MisterCat100 <savingleb2007@mail.ru>
2023-07-21 00:25:06 +0300

    L1-T5 Gagarin + time

--- src/main/kotlin/Lesson1/task3.kt
+++ src/main/kotlin/Lesson1/task3.kt
@@ -1,18 +1,17 @@
 package Lesson1
 
 fun main() {
-    val flightYear = 1961
-    var hour = 9
-    var minute = 7
+    val flightYear = "1961"
+    var hour = "09"
+    var minute = "07"
+
     println(flightYear)
     println(hour)
     println(minute)
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     print(hour)
     print(":")
     println(minute)
-
-
 
 }
