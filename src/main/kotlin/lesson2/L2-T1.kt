425ff064c45da0902d6ec09dea4c994db652ce66
MisterCat100 <savingleb2007@mail.ru>
2023-07-21 19:30:36 +0300

    L2-T1 common arifmetics

---
+++ src/main/kotlin/lesson2/L2-T1.kt
@@ -0,0 +1,11 @@
+package lesson2
+
+fun main() {
+    val rate1 = 3.0f
+    val rate2 = 4.0f
+    val rate3 = 3.0f
+    val rate4 = 5.0f
+    val answer = (rate1 + rate2 + rate3 + rate4) / 4
+    print(answer)
+
+}
