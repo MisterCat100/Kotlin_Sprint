8079c651198cacf2b7e5ddc1c30dccce6104cd2d
MisterCat100 <savingleb2007@mail.ru>
2023-07-24 23:52:11 +0300

    Just testing

--- src/main/kotlin/TESTING.kt
+++ src/main/kotlin/TESTING.kt
@@ -1,8 +1,4 @@
 fun main(args: Array<String>) {
-    var counter1 = 5
-    var counter2 = 5
-    counter1++
-    ++counter2
-    println(counter1++)
-    println(counter1)
+    val str = "abcd"
+    str += str
 }
