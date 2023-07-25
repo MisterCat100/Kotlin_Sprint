152ed5d8ca113c1415696b0c82f808dbd2492b27
MisterCat100 <savingleb2007@mail.ru>
2023-07-25 16:49:29 +0300

    L3-T1 greetings at day and evening

--- src/main/kotlin/lesson3/L3-T1.kt
+++ src/main/kotlin/lesson3/L3-T1.kt
@@ -2,13 +2,10 @@ package lesson3
 // урок-3 задача-1
 
 fun main() {
-    var greetingsDay = "Добрый день, "
-    var greetingsEvening = "Добрый вечер, "
+    val greetingsDay = "Добрый день"
+    val greetingsEvening = "Добрый вечер"
     val user = "уважаемый клиент!"
 
-    greetingsDay += user
-    greetingsEvening += user
-
-    println(greetingsDay)
-    print(greetingsEvening)
+    println("$greetingsDay, $user")
+    print("$greetingsEvening, $user")
 }
