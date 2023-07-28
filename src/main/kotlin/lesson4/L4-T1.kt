00376b181fff48389e15d378025d0e23e51894b9
MisterCat100 <savingleb2007@mail.ru>
2023-07-28 18:22:08 +0300

    L4-T1 free tables in restaurant

--- src/main/kotlin/lesson4/L4-T1.kt
+++ src/main/kotlin/lesson4/L4-T1.kt
@@ -1,7 +1,6 @@
 package lesson4
 // урок-4 задача-1
 
-
 fun main() {
     println(String.format("Доступность столиков на сегодня: %b\n" +
             "Доступность столиков на завтра: %b", (PLACES != USED_TABLES_TODAY), (PLACES != USED_TABLES_TOMORROW)))
